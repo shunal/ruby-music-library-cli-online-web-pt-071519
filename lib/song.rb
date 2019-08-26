bbaa8c2753fcdae8e97197c7b6846436441115f3
Steven Hunal <stevehunal@gmail.com>
2019-08-26 16:18:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
   extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
 
   @@all = []
 
   def initialize(name, artist=nil, genre=nil)
     @name = name
-    self.artist=(artist) if artist != nil
-    self.genre=(genre) if genre != nil
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
   end
 
   def self.all
@@ -20,16 +22,13 @@ class Song
 
   def save
     @@all << self
+    self
   end
 
-  def self.create(song)
-    song = self.new(song)
+  def self.create(name)
+    song = self.new(name)
     song.save
     song
-  end
-
-  def artist
-    @artist
   end
 
   def artist=(artist)
@@ -37,48 +36,23 @@ class Song
     artist.add_song(self)
   end
 
-  def genre
-    @genre
-  end
-
   def genre=(genre)
     @genre = genre
-    genre.songs << self unless genre.songs.include?(self)
-  end
-
-  def self.find_by_name(name)
-    @@all.detect do |song|
-      song.name == name
-    end
-  end
-
-  def self.find_or_create_by_name(name)
-    # @@all.detect do |song|
-    #   if song.name == name
-    #     song
-    #   else
-    #     self.create(name)
-    #   end
-    # end
-    self.find_by_name(name) || self.create(name)
-
+    genre.add_song(self)
   end
 
   def self.new_from_filename(filename)
-    array = filename.split(" - ")
+    artist_name_string = filename.split(" - ")[0]
+    song_name_string = filename.split(" - ")[1]
+    genre_name_string = filename.split(" - ")[2].chomp(".mp3")
 
-    song_name = array[1]
-    artist_name = array[0]
-    genre_name = array[2].split(".mp3").join
-
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
+    song = self.new(song_name_string)
+    song.artist = Artist.find_or_create_by_name(artist_name_string)
+    song.genre = Genre.find_or_create_by_name(genre_name_string)
+    song
   end
 
   def self.create_from_filename(filename)
-    self.new_from_filename(filename).save
+    new_from_filename(filename).save
   end
-
-
 end
