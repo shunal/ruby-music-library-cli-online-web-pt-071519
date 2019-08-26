4dc84d0debca38f01cd3334bf4b32a3cf3c986e0
Steven Hunal <stevehunal@gmail.com>
2019-08-26 15:44:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,46 +1,60 @@
 require "pry"
-class Artist 
-  attr_accessor :name
-  
+class Artist
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
   @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @songs = []
 
-  end 
-  
-  def genres 
-    @songs.collect do |song|
-      song.genre
-    end
-    
-  end 
-  
-  def add_song(song)
-    song.artist = self unless song.artist == self
-    @songs << song unless @songs.include?(song)
+  def initialize(name)
+    @name = name
+    @songs = []
   end
-  
-  def songs 
-    @songs
-  end 
-  
-  def save
-    @@all << self
-  end 
-  
-  def self.all 
+
+  def self.all
     @@all
-  end 
-  
+  end
+
   def self.destroy_all
     @@all.clear
-  end 
-  
-  def self.create(name)
-    artist = Artist.new(name)
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist)
+    artist = self.new(artist)
     artist.save
     artist
-  end 
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+    if @songs.include?(song)
+      nil
+    else
+      @songs << song
+    end
+    song
+  end
+
+  def genres
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.genre)
+        nil
+      else
+        @new_array << song.genre
+      end
+    end
+    @new_array
+  end
+
 end
