2231d4d87d3ef6c3e88f297b948d2e8f5c1a102a
Steven Hunal <stevehunal@gmail.com>
2019-08-26 15:31:52 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,37 @@
+class Genre
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def add_song(song)
+    song.genre = self unless song.genre == self
+    @songs << song unless @songs.include?(song)
+  end
+  
+  def songs 
+    @songs 
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end 
+end
---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+class Song 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name, artist=nil, genre=nil)
+    @name = name 
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end 
+  
+  def genre 
+    @genre 
+  end 
+  
+  def genre=(genre)
+    @genre= genre
+    if self.genre != nil
+      @genre.add_song(self)
+    end
+    @genre
+
+  end
+  
+  def artist 
+    @artist 
+  end 
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end 
+end 
