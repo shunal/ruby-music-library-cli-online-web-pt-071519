feb99251e8540ef490c315e0e0ae768f98eb1dc0
Steven Hunal <stevehunal@gmail.com>
2019-08-26 15:59:19 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,37 +1,46 @@
 class Genre
-  attr_accessor :name
-
+  attr_accessor :name, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
+    @name = name
     @songs = []
-  end 
-  
-  def add_song(song)
-    song.genre = self unless song.genre == self
-    @songs << song unless @songs.include?(song)
   end
-  
-  def songs 
-    @songs 
-  end 
-  
-  def save
-    @@all << self
-  end 
-  
-  def self.all 
+
+  def self.all
     @@all
-  end 
-  
+  end
+
   def self.destroy_all
     @@all.clear
-  end 
-  
-  def self.create(name)
-    genre = Genre.new(name)
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(genre)
+    genre = self.new(genre)
     genre.save
     genre
-  end 
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
+  end
+
+
 end
